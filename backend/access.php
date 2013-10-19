e7c87cf5a167f0a4c1055d887ac3fdc54f2adf8d
nyan-cat <butthurteverywhere@yahoo.com>
2013-10-19 19:34:23 +0200

    Group parameters aliases

--- backend/access.php
+++ backend/access.php
@@ -11,18 +11,32 @@ class access
 
     function parse_evaluate($expression, $args)
     {
-        preg_match('/\A(\w+)\[([\w,]*)\]\Z/', $expression, $matches) or backend_error('bad_group', "Incorrect group expression: $expression");
+        preg_match('/\A(\w+)\[([\w:,]*)\]\Z/', preg_replace('/\s+/', '', $expression), $matches) or backend_error('bad_group', "Incorrect group expression: $expression");
         $name = $matches[1];
-        $params = explode(',', $matches[2]);
+        $bindings = explode(',', $matches[2]);
+        $params = [];
+        foreach($bindings as &$binding)
+        {
+            if(preg_match('/(\w+):(\w+)/', $binding, $matches))
+            {
+                $binding = ['param' => $matches[1], 'arg' => $matches[2]];
+            }
+            else
+            {
+                $binding = ['param' => $binding, 'arg' => $binding];
+            }
+            $params[] = $binding['param'];
+        }
+
         $id = group::make_id($name, $params);
         isset($this->groups[$id]) or backend_error('bad_group', "Unknown group: $id");
 
         $matched = [];
 
-        foreach($params as $param)
+        foreach($bindings as $binding)
         {
-            isset($args[$param]) or backend_error('bad_group', "Not enough arguments to evaluate group $id");
-            $matched[$param] = $args[$param];
+            isset($args[$binding['arg']]) or backend_error('bad_group', "Not enough arguments to evaluate group $id");
+            $matched[$binding['param']] = $args[$binding['arg']];
         }
 
         return $this->groups[$id]->evaluate($matched);
